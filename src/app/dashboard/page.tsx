5948baddfeb23e143f218baa0c0e6104f76b6c33
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2025-06-01 18:40:31 -0300

    updated

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,7 @@
 import { DonationTable } from "./_components/donates";
 import { Stats } from "./_components/analytics";
-import { auth } from "@/lib/auth"
-import { redirect } from "next/navigation";
 export default async function Dashboard() {
-  const session = await auth();
-
-  if(!session?.user){
-    redirect("/")
-  }
-
-  console.log(session.user)
+ 
 
   return (
     <div className="p-4">
